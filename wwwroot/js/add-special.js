5577a3367366ab6a6190824bddbe9ad5ad8deced
Werner Griesel <wernerg@dotslash.co.za>
2018-01-24 16:32:15 +0200

    First

---
+++ wwwroot/js/add-special.js
@@ -0,0 +1,38 @@
+﻿'use strict';
+
+// Disable Dropzone auto discover
+if($('#dropzone-upload')[0]) {
+    Dropzone.autoDiscover = false;
+}
+
+$(document).ready(function () {
+	
+	// Drag n Drop file upload (DropzoneJs)
+    if($('#dropzone-upload')[0]) {
+        $('#dropzone-upload').dropzone({
+            url: "/file/post",
+            addRemoveLinks: true
+        });
+    }
+    
+    // Date start
+    if($('.date-picker-start')[0]) {
+        $('.date-picker-start').flatpickr({
+            enableTime: false,
+            dateFormat: "d.m.Y",
+            nextArrow: '<i class="fas fa-arrow-right" />',
+            prevArrow: '<i class="fas fa-arrow-left" />'
+        });
+    }
+    
+    // Date end
+    if($('.date-picker-end')[0]) {
+        $('.date-picker-end').flatpickr({
+            enableTime: false,
+            dateFormat: "d.m.Y",
+            nextArrow: '<i class="fas fa-arrow-right" />',
+            prevArrow: '<i class="fas fa-arrow-left" />'
+        });
+    }
+    
+});
---
+++ wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+
+// Page Loader
+$(window).on('load', function () {
+    setTimeout(function () {
+        $('.page-loader').fadeOut();
+    }, 500);
+});
+
+// Navigation ACTIVE class
+$(document).ready(function(){
+	$('.navbar-nav li a[href="' + document.location.pathname + '"]').parent('li').addClass('active');
+});
+
+// We put modals out of wrapper to working properly
+$(document).ready(function(){
+	$('.modal').appendTo("body");
+});
+
+// Tooltips and Popovers
+$(document).ready(function(){
+  $('[data-toggle="popover"]').popover();
+  $('[data-toggle="tooltip"]').tooltip();
+  $('[rel="tooltip"]').tooltip();
+});
